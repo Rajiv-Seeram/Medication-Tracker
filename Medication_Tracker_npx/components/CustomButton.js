a3a51b67e0d01a76023df9ef7d6df4daf91ec6ed
Rajiv <Seeramrajiv5@gmail.com>
2026-07-23 19:41:45 -0400

    CustomButton component created

---
+++ Medication_Tracker_npx/components/CustomButton.js
@@ -0,0 +1,26 @@
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+export default function CustomButton({onPress, title}) {
+    return (
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+            <Text style={styles.buttonText}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        backgroundColor: "#606C38",
+        borderRadius: 15,
+        paddingLeft: 80,
+        paddingRight: 80,
+        paddingTop: 16,
+        paddingBottom: 16,
+        
+    },
+    buttonText: {
+        alignItems: 'center',
+        color: "#F5F5F5"
+    }
+});
